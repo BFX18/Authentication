01accb626fc311aae4f79ae58422dae30bbe0da8
Syekh Guntur AP <syekh.guntur10@gmail.com>
2022-08-31 21:04:02 +0700

    upgrade Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ mongoose.connect("mongodb://localhost:27017/userDB")
 const userSchema = new mongoose.Schema ({
    email: String,
    password: String,
-   googleId: String
+   googleId: String,
+   secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -94,11 +95,40 @@ app.get("/login", function(req, res){
  });
 
  app.get("/secrets", function(req, res){
+   User.find({"secret": {$ne: null}}, function(err, foundUsers){
+      if (err){
+         console.log(err);
+      } else {
+         if (foundUsers) {
+            res.render("secrets", {usersWithSecrets: foundUsers});
+         }
+      }
+   });
+ });
+
+ app.get("/submit", function(req, res){
    if (req.isAuthenticated()){
-      res.render("secrets");
+      res.render("submit");
    } else {
       res.redirect("/login");
    }
+ });
+
+ app.post("/submit", function(req, res){
+   const submittedSecret = req.body.secret;
+
+   User.findById(req.user.id, function(err, foundUser){
+      if (err) {
+         console.log(err);
+      } else {
+         if (foundUser) {
+            foundUser.secret = submittedSecret;
+            foundUser.save(function(){
+               res.redirect("/secrets");
+            });
+         }
+      }
+   });
  });
 
  app.get("/logout", function(req, res){
